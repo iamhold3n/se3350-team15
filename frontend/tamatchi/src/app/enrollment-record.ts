0133f9b075815095e30f3deacf85c8841947bcf3
jchou25101 <jchou22@uwo.ca>
2021-02-04 12:43:40 -0500

    Basic Info display
    -Now reads hard-coded placeholder data and displays it
    -Not yet able to edit the hours to allocate

---
+++ frontend/tamatchi/src/app/enrollment-record.ts
@@ -0,0 +1,8 @@
+export interface EnrollmentRecord{
+    "course_id": string;
+    "instructor": string;
+    "enrollment_prev": number;
+    "ta_hr_prev": number;
+    "enrollment_current": number;
+    "allocated_hrs":number; //Potentially
+}

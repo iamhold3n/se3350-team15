28ad0f4fcf2fc2bd6580a7feaa5f53c0cd8bc284
Ariya <57851464+StormingChaos@users.noreply.github.com>
2021-02-08 15:26:14 -0500

    Added Confirmation Button
    + added a button to confirm changes to the rankings list
    + *Button does not actually do anything

--- frontend/tamatchi/src/app/ranking/ranking.component.ts
+++ frontend/tamatchi/src/app/ranking/ranking.component.ts
@@ -14,7 +14,8 @@ export class RankingComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  //list of ta names to be ranked
+  //static list of ta names to be ranked 
+  //TODO: get list of tas from database
   talist = [
     "ta1", 
     "ta2", 
@@ -24,5 +25,11 @@ export class RankingComponent implements OnInit {
   //This method will reorder the array when items are dragged around
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.talist, event.previousIndex, event.currentIndex);
+  }
+
+  //This method will save the ranking changes and send the updated list to the database
+  //TODO: Save changes to rankings
+  confirmRankings() {
+    alert("Changes Saved *not really");
   }
 }

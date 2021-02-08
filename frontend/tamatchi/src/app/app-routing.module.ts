8348e1e18fc82f1a96b8b247f39138e1588fe8ca
linahaljarad <lalhamou@uwo.ca>
2021-02-08 14:11:26 -0500

    Added front-end pages course-info and TA form

--- frontend/tamatchi/src/app/app-routing.module.ts
+++ frontend/tamatchi/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseInformationComponent } from './course-information/course-information.component';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'course-information', component: CourseInformationComponent},
+  {path: 'form', component: FormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

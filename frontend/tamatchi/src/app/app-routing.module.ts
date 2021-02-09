62671820562e1c5c262acb2658ca27fc50c38b23
jchou25101 <jchou22@uwo.ca>
2021-02-09 18:47:14 -0500

    Merge Linah & John's front-end pages
    -Routing module can now access both John and LInah's front-end pages

--- frontend/tamatchi/src/app/app-routing.module.ts
+++ frontend/tamatchi/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllocateTaHoursComponent } from './allocate-ta-hours/allocate-ta-hours.component';
+import { AssignTaComponent } from './assign-ta/assign-ta.component';
 import { CourseInformationComponent } from './course-information/course-information.component';
 import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
   {path: 'course-information', component: CourseInformationComponent},
-  {path: 'form', component: FormComponent}
+  {path: 'form', component: FormComponent},
+  {path: 'allocate-ta-hours', component:AllocateTaHoursComponent},
+  {path: 'assign-ta', component:AssignTaComponent}
 
 ];
 

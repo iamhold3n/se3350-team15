bf5bce6899f2585be3b92ace7e794486d8765e97
jchou25101 <jchou22@uwo.ca>
2021-02-09 19:29:15 -0500

    Merged Adriana's front-end pages

--- frontend/tamatchi/src/app/app-routing.module.ts
+++ frontend/tamatchi/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AllocateTaHoursComponent } from './allocate-ta-hours/allocate-ta-hours.
 import { AssignTaComponent } from './assign-ta/assign-ta.component';
 import { CourseInformationComponent } from './course-information/course-information.component';
 import { FormComponent } from './form/form.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 const routes: Routes = [
   {path: 'course-information', component: CourseInformationComponent},
   {path: 'form', component: FormComponent},
   {path: 'allocate-ta-hours', component:AllocateTaHoursComponent},
-  {path: 'assign-ta', component:AssignTaComponent}
+  {path: 'assign-ta', component:AssignTaComponent},
+  {path: 'ranking', component:RankingComponent}
 
 ];
 

fdc8c1afa2afea747fa05d40211ec2c4418a1217
iamhold3n <matt@hold3n.ca>
2021-03-04 22:17:14 -0500

    Modify Course Questions
    - Implemented the front end ability to modify course questions
    - Added Tamatchi mascot

--- frontend/tamatchi/src/app/course-information/course-information.component.ts
+++ frontend/tamatchi/src/app/course-information/course-information.component.ts
@@ -20,6 +20,8 @@ export class CourseInformationComponent implements OnInit {
 
   public rows: Array<{courseCode: string, courseName: string, lecHours: number,labOrTutHours: number, sections: number }> = [];
 
+  public courseSelected = {courseCode: null, courseName: null, questions: [] };
+
   buttonClicked() {
     this.rows.push( {courseCode: this.courseCode, courseName: this.courseName, lecHours: this.lecHours, labOrTutHours: this.labOrTutHours, sections: this.sections} );
 
@@ -39,6 +41,50 @@ export class CourseInformationComponent implements OnInit {
 
 
   ngOnInit(): void {
+  }
+
+  courseQuestions(course) {
+    // query server for existing course questions
+
+    // dummy data for now
+    this.courseSelected.questions = [];
+    this.courseSelected.courseCode = course;
+    this.courseSelected.courseName = 'Intro Course';
+    this.courseSelected.questions.push('Know Javascript?');
+
+    // popup effect
+    const darkened = document.getElementById('darkened');
+    const popup = document.getElementById('popup');
+    darkened.style.display = 'block';
+    popup.style.display = 'block';
+  }
+
+  addQuestion() {
+    this.courseSelected.questions.push('');
+  }
+
+  removeQuestion(i) {
+    this.courseSelected.questions.splice(i, 1);
+  }
+
+  saveQuestions() {
+    // send updated questions to server
+
+    // for now, just print to console
+    console.log(this.courseSelected);
+    
+    this.closeQuestions();
+  }
+
+  closeQuestions() {
+    const darkened = document.getElementById('darkened');
+    const popup = document.getElementById('popup');
+    darkened.style.display = 'none';
+    popup.style.display = 'none';
+  }
+
+  trackByFn(index, item) { // prevent heavy DOM manipulation when editing questions
+    return index;
   }
 
 }

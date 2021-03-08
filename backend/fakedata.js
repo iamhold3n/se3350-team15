cf11b877d57306aee7b8d2e33821bd6398c1b4f8
iamhold3n <matt@hold3n.ca>
2021-03-07 19:02:42 -0500

    Fake Data Script
    - Fixed inconsistencies from mockaroo generated data by creating script.

---
+++ backend/fakedata.js
@@ -0,0 +1,175 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./apikey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+const db = admin.firestore();
+const faker = require('faker');
+
+// hard coding parameters, may add command line arguments later
+let courseNum = 10;
+let instructorsNum = 15;
+let enrolhrsNum = 10;
+let applicantsNum = 50;
+let allocationNum = 10;
+
+// =============
+// GENERATE DATA
+// =============
+let courses = [];
+for (let i = 0; i < courseNum; i++) {
+    courses.push({
+        courseCode: faker.random.arrayElement(['SE', 'ECE']) + (Math.floor(Math.random() * 3) + 1) + Math.floor(Math.random() * 3) + Math.floor(Math.random() * 9) + Math.floor(Math.random() * 9),
+        courseName: faker.random.arrayElement(['Intro ', 'Theoretical ', 'Software ']) + faker.random.arrayElement(['Design', 'Engineering', 'Foundations']),
+        labOrTutHrs: Math.floor(Math.random() * 3) + 1,
+        lecHrs: Math.floor(Math.random() * 4) + 1,
+        questions: uniqueQuestions(),
+        sec: Math.floor(Math.random() * 4) + 1
+    })
+}
+
+let instructors = [];
+for (let i = 0; i < instructorsNum; i++) {
+    instructors.push({
+        email: faker.internet.email(),
+        name: faker.name.firstName() + ' ' + faker.name.lastName()
+    })
+}
+
+let enrolhrs = [];
+for (let i = 0; i < enrolhrsNum; i++) {
+    enrolhrs.push({
+        courseCode: courses[i].courseCode,
+        currEnrol: Math.floor(Math.random() * 150) + 100,
+        labOrTutHrs: courses[i].labOrTutHrs,
+        prevEnrol: Math.floor(Math.random() * 150) + 100,
+        prevHrs: Math.floor(Math.random() * 20) + 15,
+    })
+}
+
+let applicants = [];
+for (let i = 0; i < applicantsNum; i++) {
+    let c = Math.floor(Math.random() * (courseNum - 1));
+    applicants.push({
+        course: courses[c].courseCode,
+        email: faker.internet.email(),
+        name: faker.name.firstName() + ' ' + faker.name.lastName(),
+        questions: questionAnswers(c),
+        status: Math.floor(Math.random() * 3) + 1
+    })
+}
+
+let allocation = [];
+for (let i = 0; i < allocationNum; i++) {
+    let hours = Math.floor(Math.random() * 20) + 15;
+    allocation.push({
+        course: courses[i].courseCode,
+        currEnrol: enrolhrs[i].currEnrol,
+        currHrs: hours,
+        estHrs: (Math.floor(enrolhrs[i].currEnrol / enrolhrs[i].prevEnrol * enrolhrs[i].prevHrs)),
+        instructor: instructors[Math.floor(Math.random() * (instructorsNum - 1))].name,
+        labOrTutHrs: courses[i].labOrTutHrs,
+        labSections: Math.floor(Math.random() * 4),
+        prevEnrol: enrolhrs[i].prevEnrol,
+        prevHrs: enrolhrs[i].prevHrs,
+        tutSections: Math.floor(Math.random() * 4)
+    })
+}
+
+function uniqueQuestions() {
+    let q = new Set();
+    let m = Math.floor(Math.random() * 4) + 1;
+    while(q.size < m) {
+        q.add(faker.random.arrayElement(['Know Java?','Know Javascript?','Know Matlab?','Know C++?','Know C?','Know Rust?','Know Python?','Know Ruby?','Know R?','Teaching certificate?','Know DBMS?']));
+    }
+
+    return Array.from(q);
+}
+
+function questionAnswers(c) {
+    let qa = [];
+    courses[c].questions.forEach(q => {
+        qa.push({
+            question: q,
+            answer: faker.random.boolean()
+        })
+    })
+
+    return qa;
+}
+
+// ===========
+// UPLOAD DATA
+// ===========
+courses.forEach(x => {
+    db.collection("courses").add({
+        courseCode: x.courseCode,
+        courseName: x.courseName,
+        labOrTutHrs: x.labOrTutHrs,
+        lecHrs: x.lecHrs,
+        questions: x.questions,
+        sec: x.sec
+    }).then(x => {
+        console.log("Added course")
+    }).catch(x => {
+        console.log("Error: ", error);
+    })
+})
+
+instructors.forEach(x => {
+    db.collection("instructors").add({
+        name: x.name,
+        email: x.email
+    }).then(x => {
+        console.log("Added instructor")
+    }).catch(e => {
+        console.log("Error: ", e);
+    })
+})
+
+enrolhrs.forEach(x => {
+    db.collection("enrolhrs").add({
+        courseCode: x.courseCode,
+        labOrTutHrs: x.labOrTutHrs,
+        prevEnrol: x.prevEnrol,
+        prevHrs: x.prevHrs,
+        currEnrol: x.currEnrol
+    }).then(x => {
+        console.log("Added course info");
+    }).catch(e => {
+        console.log("Error: " + e);
+    })
+})
+
+applicants.forEach(x => {
+    db.collection("applicants").add({
+        course: x.course,
+        name: x.name,
+        email: x.email,
+        status: x.status,
+        questions: x.questions
+    }).then(x => {
+        console.log("Added applicant");
+    }).catch(e => {
+        console.log("Error: " + e);
+    })
+})
+
+allocation.forEach(x => {
+    db.collection("allocation").add({
+        instructor: x.instructor,
+        course: x.course,
+        labSections: x.labSections,
+        labOrTutHrs: x.labOrTutHrs,
+        tutSections: x.tutSections,
+        prevEnrol: x.prevEnrol,
+        prevHrs: x.prevHrs,
+        currEnrol: x.currEnrol,
+        estHrs: x.estHrs,
+        currHrs: x.currHrs
+    }).then(x => {
+        console.log("Added allocation");
+    }).catch(e => {
+        console.log("Error: " + e);
+    })
+})

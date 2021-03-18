a370f340dfa3f3f5fecad4ffbb2d765c037c88f2
iamhold3n <matt@hold3n.ca>
2021-03-18 16:11:39 -0400

    Minor Fix (Allocate Hours)
    Fixed a minor issue with the backend saving the hours as a string rather than an integer in Firestore.

--- backend/index.js
+++ backend/index.js
@@ -190,7 +190,7 @@ app.post('/api/allocation', [
 ], (req, res) => {
   req.body.forEach(e => {
     db.collection('allocation').where('course', '==', e.course).get().then(x => {
-        x.forEach(d => db.collection('allocation').doc(d.id).update({ currHrs : e.currHrs }) )
+        x.forEach(d => db.collection('allocation').doc(d.id).update({ currHrs : parseInt(e.currHrs) }) )
     })
   })
 

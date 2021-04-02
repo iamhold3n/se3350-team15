577849dd631fc1da06c33fa49e7acae97c4d4b8a
jchou25101 <jchou22@uwo.ca>
2021-04-02 15:15:26 -0400

    Cleaning up the merge

--- frontend/tamatchi/src/app/ranking/ranking.component.ts
+++ frontend/tamatchi/src/app/ranking/ranking.component.ts
@@ -6,8 +6,6 @@ import { TaInfoComponent } from '../ta-info/ta-info.component';
 
 import {AuthService} from '../auth.service';
 
-import { TaInfoComponent } from '../ta-info/ta-info.component';
-
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.component.html',
